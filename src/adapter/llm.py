4ad52ffd5d1525db81300cb43444815674c3c6d1
mike-toleds <mike.toleds@gmail.com>
2024-12-11 00:39:05 +0800

    - prompt engineering update - updated sample_response.md

--- src/adapter/llm.py
+++ src/adapter/llm.py
@@ -15,7 +15,7 @@ class LlmService:
         template = """
         You are an assistant for question-answering tasks. 
         Use ONLY the following piece of context below to answer the question in detail and DO NOT add any information outside of 
-        it. Can you provide information about the \"document name\" and  \"page\" at the end of your response as references.  
+        it. Can you provide information about the \"document source\" and  \"page number\" if present at the end of your response as references.  
         
         If context information provided is missing, just say you don't know, don't make up an answer.        
         
